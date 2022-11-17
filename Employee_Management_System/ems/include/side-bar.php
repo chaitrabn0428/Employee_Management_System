d991dce0bab287304051eed84aca387d95d14005
Jigya Taneja <94694052+Jigya1@users.noreply.github.com>
2022-11-17 09:51:13 +0530

    Add files via upload

---
+++ Employee_Management_System/ems/include/side-bar.php
@@ -0,0 +1,56 @@
+<!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+          <i class="fa fa-calendar-o"></i>
+          <span>Manage Projects</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          
+          <a class="dropdown-item" href="manageproject.php">View Project</a>
+          
+      
+         
+        </div>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+          <i class="fa fa-calendar-o"></i>
+          <span>Manage Leave</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          
+          <a class="dropdown-item" href="add-leave.php">Apply Leave</a>
+          <a class="dropdown-item" href="view-leave.php">View Leave</a>
+      
+         
+        </div>
+      </li>
+     <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+          <i class="fa fa-plane"></i>
+          <span>Manage Tour</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          
+          <a class="dropdown-item" href="add-tour.php">Apply Tour</a>
+          <a class="dropdown-item" href="view-tour.php">View Tour</a>
+      
+         
+        </div>
+        
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link " href="payroll.php" aria-haspopup="true" aria-expanded="false" style="color: white;">
+          <i class="fa fa-calendar-o"></i>
+          <span>Payroll</span>
+        </a>
+      </li>
+    </ul>
